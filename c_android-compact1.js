0cb7bd45230cc00f8bf83567b7d8ccec3cde836b
imn0761 <imn0761@gmail.com>
2024-11-18 21:02:49 +0700

    first commit

---
+++ c_android-compact1.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+import './c_android-compact1.css'
+
+const CAndroidCompact1 = (props) => {
+  return (
+    <div className="c_android-compact1-frame">
+      <img
+        src="./assets/467db3e5e3356637c660731a712b1fae.svg"
+        alt="rectangle"
+        width={297}
+        height={61}
+        className="c_android-compact1-rectangle"
+      />
+      <div className="c_android-compact1-text">
+        <p className="c_android-compact1-text01">Enter your Email address</p>
+      </div>
+      <img
+        src="./assets/a009f801edeaf261a717f1c40ac0759b.svg"
+        alt="rectangle"
+        width={297}
+        height={61}
+        className="c_android-compact1-rectangle1"
+      />
+      <div className="c_android-compact1-text02">
+        <p className="c_android-compact1-text03">Enter your Password</p>
+      </div>
+      <div className="c_android-compact1-text04">
+        <p className="c_android-compact1-text05">Email Address</p>
+      </div>
+      <div className="c_android-compact1-text06">
+        <p className="c_android-compact1-text07">Password</p>
+      </div>
+      <img
+        src="./assets/679667bd70828d360bfc9ed2c9ba4f5f.png"
+        alt="rectangle"
+        width={41}
+        height={26}
+        className="c_android-compact1-rectangle2"
+      />
+      <img
+        src="./assets/dfa4e03d52b6326e5354775d4feaa136.png"
+        alt="rectangle"
+        width={35}
+        height={30}
+        className="c_android-compact1-rectangle3"
+      />
+      <div className="c_android-compact1-text08">
+        <p className="c_android-compact1-text09">Login to your account</p>
+      </div>
+      <div className="c_android-compact1-text10">
+        <p className="c_android-compact1-text11">Sign Up</p>
+      </div>
+      <div className="c_android-compact1-text12">
+        <p className="c_android-compact1-text13">12.00</p>
+      </div>
+      <div className="c_android-compact1-text14">
+        <p className="c_android-compact1-text15">Don’t have any account?</p>
+      </div>
+      <img
+        src="./assets/d983769cb7b7fbe65ea99c48bdc7dcc3.jpg"
+        alt="rectangle"
+        width={282}
+        height={167}
+        className="c_android-compact1-rectangle4"
+      />
+    </div>
+  )
+}
+
+export default CAndroidCompact1
